49a4c13d049299ee8a3f532021822d350121680d
Pedro Silva <pedro_rike@hotmail.com.br>
2023-11-02 00:12:09 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://notes-serverless-app.com',
     defaultCommandTimeout: 10000,
     chromeWebSecurity: false,
     env: {
       viewportWidthBreakpoint: 768,
     },
+    projectId: 'v4pnms'
   },
 })
